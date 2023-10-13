01bd6709bf8a32769ffde32ee8c7edd36e21b946
Daniel Khoury <77999379+Dan-Kho@users.noreply.github.com>
2023-10-12 18:07:25 -0700

    Complete getComputerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // script.js
 
 function getComputerChoice () {
-
+    let rand = Math.floor(Math.random() * 3);
+    let choice = (rand == 0) ? "Rock" : (rand == 1) ? "Paper" : "Scissors";
+    return choice;
 }
