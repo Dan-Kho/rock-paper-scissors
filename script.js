f57fd700e5e959845f7f9dae7a7df14d82eb6bef
Daniel Khoury <77999379+Dan-Kho@users.noreply.github.com>
2023-10-12 19:27:55 -0700

    Add rock paper scissors rules in playRound

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ function playRound (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection == computerSelection) {
-        return console.log(`You Tied! You both selected ${computerSelection[0].toUpperCase()}${computerSelection.slice(1)}.`);
-    }
+        return `You Tied! You both selected ${computerSelection[0].toUpperCase()}${computerSelection.slice(1)}.`;
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") return "You Lose! Paper beats Rock.";
+        else return "You Win! Rock beats Scissors.";
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") return "You Win! Paper beats Rock.";
+        else return "You Lose! Scissors beats Paper.";
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") return "You Lose! Rock beats Scissors.";
+        else return "You Win! Scissors beats Paper.";
+    } 
 
-    return console.log("Try Again.");;
+    return console.log("Try Again.");
 }
 
 let playerSelection = "PaPer";
